9314a00b3a83d304d72b317673eddab4132ce6f3
isaactan14 <x22207872@student.ncirl.ie>
2023-11-05 23:16:19 +0000

    First draft commit

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Welcome to ETH Blockchain Donation Portal', name:null });
+});
+
+router.post('/', function(req, res, next) {
+
+  res.render('index', { title: 'Welcome to ETH Blockchain Donation Portal', name:req.body.name });
+});
+
+router.get('/page1', function(req, res, next) {
+  res.render('page1', { title: 'Welcome to ETH Blockchain Donation Portal', name:null });
+});
+
+router.get('/page2', function(req, res, next) {
+  res.render('page2', { title: 'Welcome to ETH Blockchain Donation Portal', name:null });
+});
+
+router.get('/page3', function(req, res, next) {
+  res.render('page3', { title: 'Welcome to ETH Blockchain Donation Portal', name:null });
+});
+
+
+module.exports = router;
